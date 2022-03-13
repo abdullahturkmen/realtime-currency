8b2659e6fb21f058d0fa3fcdb85de0c095ee9f5a
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2022-03-13 21:52:58 +0300

    detail page added

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
 
+app.get('/:userId', (req, res) => {
+  res.sendFile(__dirname + '/detail.html')
+})
+
+
+io.on('connection', (socket) => {
+
+  socket.on('getAllCurrency', msg => {
 
     setInterval(function () {
         
@@ -25,22 +31,43 @@ io.on('connection', (socket) => {
             axios.post('https://cizrekuyumculardernegi.org/Kurapi/curency-data.json', {})
             .then(function (secondResponse){
               //console.log(secondResponse.data.currency);
-              io.emit('chat message', response.data.data);
-              io.emit('currency data', secondResponse.data.currency);
+              io.emit('getAllCurrency', response.data.data);
+              io.emit('getCurrencyFilter', secondResponse.data.currency);
             });
-
 
         });
 
       }, 1000);
 
+  });
 
+
+  socket.on('getSelectCurrency', msg => {
+
+    setInterval(function () {
+        
+        axios({
+          method: 'post',
+          headers: {'X-Requested-With': 'XMLHttpRequest'},
+          url: 'https://www.haremaltin.com/ajax/all_prices'
+        })
+        .then(function (response) {
+            //console.log(response);
+
+            axios.post('https://cizrekuyumculardernegi.org/Kurapi/curency-data.json', {})
+            .then(function (secondResponse){
+              console.log(response);
+              io.emit('getSelectCurrency', response.data.data);
+              io.emit('getSelectCurrency', secondResponse.data.currency);
+            });
+
+        });
+
+      }, 1000);
 
   });
+
 });
-
-
-
 
 
 
